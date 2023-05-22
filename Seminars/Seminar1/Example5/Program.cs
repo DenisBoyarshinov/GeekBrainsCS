bf67d44463d3573886762275c8fb800bfcd58e2b
Denis Boyarshinov <dropthegunboris@mail.ru>
2023-05-22 22:47:55 +0300

    task5

---
+++ Seminars/Seminar1/Example5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+if ((num>99)&&(num<1000))
+{
+int num2 = num%10;
+Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine("Обман");
+}
