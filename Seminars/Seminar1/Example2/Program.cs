2c78b6ca5a88d09f7955e79657ccd177fefc5b29
Denis Boyarshinov <dropthegunboris@mail.ru>
2023-05-22 22:38:09 +0300

    task2

---
+++ Seminars/Seminar1/Example2/Program.cs
@@ -0,0 +1,14 @@
+﻿// (ЗАДАНИЕ2) Программа которая на вход принемает два числа, проверяет является ли первое число квадратом второго
+
+Console.WriteLine("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int num2=Convert.ToInt32(Console.ReadLine());
+if(num==num2*num2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет"); 
+}
