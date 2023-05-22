0c98587073515fe11aa05bdcc9be0e63e7796c09
Denis Boyarshinov <dropthegunboris@mail.ru>
2023-05-22 22:29:20 +0300

    task1

---
+++ Seminars/Seminar1/Example1/Program.cs
@@ -0,0 +1,6 @@
+﻿//(ЗАДАНИЕ1)Программа которая возводит число в квадрат
+
+Console.WriteLine("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+int num2= num*num;
+Console.WriteLine(num2);
