72635dabaa5baaf7643743d8bfeac64acb6a3818
Denis Boyarshinov <dropthegunboris@mail.ru>
2023-05-27 00:01:40 +0300

    add new Exercises

---
+++ Lections/Lection1/Exercise3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11,21,31,41,15,61,17,41,18,19};
+int n = array.Length;
+int find = 41;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
