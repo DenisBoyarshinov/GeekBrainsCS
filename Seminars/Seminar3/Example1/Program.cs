d9d2aa0e25ccb9bf9222f5fe84c3c6a6d6686c71
Denis Boyarshinov <dropthegunboris@mail.ru>
2023-06-02 17:14:32 +0300

    another seminar is over

---
+++ Seminars/Seminar3/Example1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты точки (X и Y), причем X не равен нулю
+// и Y не равен нулю. И выдает номер четверти плоскости,
+// в которой находится эта точка.
+
+Console.WriteLine("Введите число");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if ((X>0) && (Y>0))
+{
+    Console.WriteLine("Первая четверть");
+}
+else if ((X<0) && (Y>0))
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if ((X<0) && (Y<0))
+{
+    Console.WriteLine("Третья четверть");
+}
+else
+{
+    Console.WriteLine("Четвертая четверть");
+}
---
+++ Seminars/Seminar3/Example2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат
+// точек в этой четверти (x и y)
+
+// Console.WriteLine("Введите число от 1 до 4");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num == 1)
+// {
+//     Console.WriteLine("x>0 и y>0");
+// }
+// else if (num == 2)
+// {
+//     Console.WriteLine("x<0 и y>0");
+// }
+// else if (num == 3)
+// {
+//     Console.WriteLine("x<0 и y<0");
+// }
+// else if (num == 4)
+// {
+//     Console.WriteLine("x>0 и y<0");
+// }
+// else
+// {
+//     Console.WriteLine("Не верное число");
+// }
+
+
+
+Console.WriteLine("Введите число от 1 до 4");
+int num = Convert.ToInt32(Console.ReadLine());
+switch(num)
+{
+    case 1:
+    {
+        Console.WriteLine("x>0 и y>0");
+        break;
+    }
+
+    case 2:
+    {
+        Console.WriteLine("x<0 и y>0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("x<0 и y<0");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("x>0 и y<0");
+        break;
+    }
+    default:
+    {
+    Console.WriteLine("Не верное число");
+    break;
+    }
+}
