84d1746dacc7e630e5d5d345c494881f21a11534
Denis Boyarshinov <dropthegunboris@mail.ru>
2023-06-07 13:12:37 +0300

    another lection is over

---
+++ Lections/Lection3/Exercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[,] matrix = new int [3, 4];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Console.Write($"{matr [i, j]} ");
+        }
+        Console.WriteLine();
+}
+}
+void FillArray(int[,] matr) 
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix [i,j] = new Random().Next(1,10);
+    }
+}
+}
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
