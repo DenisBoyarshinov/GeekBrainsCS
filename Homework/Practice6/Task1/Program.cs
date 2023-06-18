f42b918b6e569b264e8dd9bae51aefa273578fb7
Denis Boyarshinov <dropthegunboris@mail.ru>
2023-06-18 19:26:51 +0300

    Task 41 and 43

---
+++ Homework/Practice6/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько нужно чисел?");
+int count = Convert.ToInt32(Console.ReadLine());
+int pos = 0;
+
+Console.WriteLine("Введите числа");
+while (count > 0)
+{
+    int enter = Convert.ToInt32(Console.ReadLine());
+    if (enter > 0)
+    {
+        pos++;
+    }
+    count--;
+}
+Console.WriteLine();
+Console.Write("Колличество чисел больше нуля"+ " " + pos);
